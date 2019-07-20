a1a96745226ca81197d02e3cb5c3c565d8c482a8
Don Vo <dontvo@gmail.com>
2019-07-19 23:18:03 -0400

    Update setup.sh
    Trying to remove sed

--- postgresql-start/setup.sh
+++ postgresql-start/setup.sh
@@ -1,7 +1,5 @@
 echo "Begin database setup..."
 
-sed -i -e "s/:dbpassword/$DB_PASSWORD/g" /opt/app-root/src/postgresql-start/setupdb.sql
-
 psql -v dbuser=$DB_USERNAME \
      -v dbpassword=$DB_PASSWORD \
      -v dbname=$POSTGRESQL_DATABASE \
