e71d5a4326521633a601533d2e13c4c72e0f751a
Don Vo <dontvo@gmail.com>
2019-07-20 00:34:53 -0400

    Trying to create user from shell script

--- postgresql-start/setup.sh
+++ postgresql-start/setup.sh
@@ -4,5 +4,6 @@ psql -v dbuser=$DB_USERNAME \
      -v dbpassword=$DB_PASSWORD \
      -v dbname=$POSTGRESQL_DATABASE \
      -v dbadmin=$POSTGRESQL_USER \
+     -c "CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';" \
      -f /opt/app-root/src/postgresql-start/setupdb.sql
      
